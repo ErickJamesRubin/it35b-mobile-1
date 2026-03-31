3a37097278ab6b6a9d2340c7c1f5734887fa2714
ErickJamesRubin <rubinerick123@gmail.com>
2026-03-31 08:41:16 +0800

    Added click me button alert

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,21 @@
+import React from 'react';
+import { IonAlert, IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
-
+       <>
+        <IonButton id="present-alert">Click Me</IonButton>
+        <IonAlert
+          trigger="present-alert"
+          header="A Short Title Is Best"
+          subHeader="A Sub Header Is Optional"
+          message="A message should be a short, complete sentence."
+          buttons={['Action']}
+        ></IonAlert>
+      </>
+    );
+  }
 export default ExploreContainer;
---
+++ src/components/NameContainer.tsx
@@ -0,0 +1,13 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const NameContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <h1>Hello World Goodbye</h1>
+    </div>
+  );
+};
+
+export default NameContainer;
