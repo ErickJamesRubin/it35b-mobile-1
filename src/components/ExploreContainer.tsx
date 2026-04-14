21430b6aa07608bc56077e17d1dfb73081e73738
Dustin Henderson <rubin.erick123@gmail.com>
2026-04-14 17:20:12 +0800

    Added functionalities in explore container

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
-import { IonAlert, IonButton } from '@ionic/react';
+import { IonButton, IonActionSheet } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-       <>
-        <IonButton id="present-alert">Click Me</IonButton>
-        <IonAlert
-          trigger="present-alert"
-          header="A Short Title Is Best"
-          subHeader="A Sub Header Is Optional"
-          message="A message should be a short, complete sentence."
-          buttons={['Action']}
-        ></IonAlert>
-      </>
-    );
-  }
+    <div id="container">
+      <IonButton id="open-action-sheet">Open</IonButton>
+      <IonActionSheet
+        trigger="open-action-sheet"
+        header="Actions"
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+          {
+            text: 'Share',
+            data: {
+              action: 'share',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ]}
+      ></IonActionSheet>
+    </div>
+  );
+};
+
 export default ExploreContainer;
