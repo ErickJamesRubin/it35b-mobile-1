3ec7d43cf12b47243fe16dad55dc915405cc048d
Dustin Henderson <rubin.erick123@gmail.com>
2026-04-14 17:42:27 +0800

    Changes made in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import FabButton from '../components/FabButton';
 
 const Home: React.FC = () => {
   return (
-
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-        <FabButton/>
-        
-      </IonContent>
-    </IonPage>
-  );
-
-  export default Home;
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+        <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Erick</IonTitle>
+    </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+ 
