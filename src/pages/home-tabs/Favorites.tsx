5e0e082893b4b6d46d084a411b19327efa5b4b22
Dustin Henderson <rubin.erick123@gmail.com>
2026-04-14 20:03:00 +0800

    Added function to favorite tabs

---
+++ src/pages/home-tabs/Favorites.tsx
@@ -0,0 +1,22 @@
+import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Favorites:React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton></IonMenuButton>
+                        </IonButtons>
+                        <IonTitle>Favorites</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+                <h1>Favorites</h1>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Favorites;
