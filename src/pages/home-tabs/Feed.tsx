e044e46f8a197aa403e55e1122daefaf3c65b85f
ErickJamesRubin <rubin.erick123@gmail.com>
2026-04-21 07:48:43 +0800

    Added list of items

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,6 +1,15 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonList } from "@ionic/react";
+import { IonButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonMenuButton, IonPage, IonTitle, IonToolbar, Ion-list } from '@ionic/react';
 
 const Feed:React.FC = () => {
+
+    const games = [
+    {name: 'Pokemon Yellow'},
+    {name: 'P<ega Man X'},
+    {name: 'Pokemon Yellow'},
+    {name: 'Pokemon Yellow'},
+    {name: 'Pokemon Yellow'}
+    ]
     return (
         <IonPage>
             <IonHeader>
@@ -13,6 +22,29 @@ const Feed:React.FC = () => {
             </IonHeader>
 
             <IonContent fullscreen>
+                <IonList>
+            <IonItem>
+                <IonLabel>Pokémon Yellow</IonLabel>
+            </IonItem>
+            <IonItem>
+                <IonLabel>Mega Man X</IonLabel>
+            </IonItem>
+            <IonItem>
+                <IonLabel>The Legend of Zelda</IonLabel>
+            </IonItem>
+            <IonItem>
+                <IonLabel>Pac-Man</IonLabel>
+            </IonItem>
+            <IonItem>
+                <IonLabel>Super Mario World</IonLabel>
+            </IonItem>
+            </IonList>
+                {games.map((item,index) => (
+                <IonItem key={index}>
+                    <IonLabel>{item.name}</IonLabel>
+                </IonItem>
+
+            ))}
                 <h1>Feed</h1>
             </IonContent>
         </IonPage>
